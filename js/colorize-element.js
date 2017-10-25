5001bf8ef66ac5de077284421d58108162192ebd
Peresmekhin Aleksey <aleksfers@icloud.com>
2017-10-25 15:37:48 +0500

    Fix colorize module

--- js/colorize-element.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-window.colorizeElement = (function () {
-  var colorizeElement = function (elem, colors, index, cb) {
-    cb(elem, colors, index);
-  };
-  return colorizeElement;
-})();
--- js/colorize.js
+++ js/colorize.js
@@ -1,42 +1,46 @@
 'use strict';
 (function () {
-  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  var setupWizard = document.querySelector('.wizard');
-  var setupWizardCoat = document.querySelector('.wizard-coat');
-  var setupWizardEyes = document.querySelector('.wizard-eyes');
-  var setupFireball = document.querySelector('.setup-fireball-wrap');
-  var colorsIndex = {
-    coat: 0,
-    eyes: 0,
-    fireball: 0
-  };
-  var incrementColorIndex = function (index, colors) {
-    return (index + 1) % colors.length;
-  };
-  var colorize = {
-    fillElement: function (elem, colors, index) {
-      elem.style.fill = colors[index];
+  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+    EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'],
+    FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'],
+    setupWizard = document.querySelector('.wizard'),
+    setupWizardCoat = document.querySelector('.wizard-coat'),
+    setupWizardEyes = document.querySelector('.wizard-eyes'),
+    setupFireball = document.querySelector('.setup-fireball-wrap'),
+    colorsIndex = {
+      coat: 0,
+      eyes: 0,
+      fireball: 0
     },
-    changeElementBackground: function (elem, colors, index) {
-      elem.style.backgroundColor = colors[index];
-    }
-  };
+    incrementColorIndex = function (index, colors) {
+      return (index + 1) % colors.length;
+    },
+    colorize = {
+      fillElement: function (elem, colors, index) {
+        elem.style.fill = colors[index];
+      },
+      changeElementBackground: function (elem, colors, index) {
+        elem.style.backgroundColor = colors[index];
+      },
+      colorizeElement: function (elem, colors, index, cb) {
+        cb(elem, colors, index);
+      }
+    };
   setupWizard.addEventListener('click', function (evt) {
     var evtTarget = evt.target;
     switch (evtTarget) {
       case setupWizardCoat:
-        window.colorizeElement(setupWizardCoat, window.setup.coatColors, colorsIndex.coat, colorize.fillElement);
-        colorsIndex.coat = incrementColorIndex(colorsIndex.coat, window.setup.coatColors);
+        colorize.colorizeElement(setupWizardCoat, COAT_COLORS, colorsIndex.coat, colorize.fillElement);
+        colorsIndex.coat = incrementColorIndex(colorsIndex.coat, COAT_COLORS);
         break;
       case setupWizardEyes:
-        window.colorizeElement(setupWizardEyes, window.setup.eyesColors, colorsIndex.eyes, colorize.fillElement);
-        colorsIndex.eyes = incrementColorIndex(colorsIndex.eyes, window.setup.eyesColors);
+        colorize.colorizeElement(setupWizardEyes, EYES_COLORS, colorsIndex.eyes, colorize.fillElement);
+        colorsIndex.eyes = incrementColorIndex(colorsIndex.eyes, EYES_COLORS);
         break;
     }
-    return false;
   });
   setupFireball.addEventListener('click', function () {
-    window.colorizeElement(setupFireball, FIREBALL_COLORS, colorsIndex.fireball, colorize.changeElementBackground);
+    colorize.colorizeElement(setupFireball, FIREBALL_COLORS, colorsIndex.fireball, colorize.changeElementBackground);
     colorsIndex.fireball = incrementColorIndex(colorsIndex.fireball, FIREBALL_COLORS);
   });
 })();
